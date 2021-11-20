4ec2fc92669dd18fa3a388450503d28d159c663a
umit <umit@fsl.cs.sunysb.edu>
2021-11-19 22:40:19 -0500

    updated license

--- benchmark/bench_math.cpp
+++ benchmark/bench_math.cpp
@@ -1,4 +1,12 @@
-// Copyright FSL Stony Brook
+/*
+ * Copyright (c) 2019- Ibrahim Umit Akgun
+ * Copyright (c) 2019- Erez Zadok
+ * Copyright (c) 2019- Stony Brook University
+ * Copyright (c) 2019- The Research Foundation of SUNY
+ *
+ * You can redistribute it and/or modify it under the terms of the Apache
+ * License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).
+ */
 
 extern "C" {
 #ifndef __APPLE__
--- examples/readahead_net_classification_training.c
+++ examples/readahead_net_classification_training.c
@@ -1,5 +1,8 @@
 /*
  * Copyright (c) 2019- Ibrahim Umit Akgun
+ * Copyright (c) 2019- Erez Zadok
+ * Copyright (c) 2019- Stony Brook University
+ * Copyright (c) 2019- The Research Foundation of SUNY
  *
  * You can redistribute it and/or modify it under the terms of the Apache
  * License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).
